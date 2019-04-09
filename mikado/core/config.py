d2e6df5669f120dde5f45007b22f002eea8715ca
Paul R Brian <paul@mikadosoftware.com>
2019-04-09 07:49:33 +0100

    Adjust to make default section invisble

--- mikado/core/config.py
+++ mikado/core/config.py
@@ -9,7 +9,14 @@ For now I am content with just passing in a location for an
 
 No mess no fuss.
 
-I amsure later on we can add default locations, envvars etc.
+I am sure later on we can add default locations, envvars etc.
+
+Because its an ini file it must have _sections_ - including the 
+'default'.  This is awkward, as I dont like the API being
+confd['default']['name'] and prefer confd['name']
+However if I add a section myself, I clearly want it namespaced
+as in confd['volumes']['data']
+
 
 usage::
 
@@ -19,7 +26,7 @@ usage::
 
 """
 import configparser
-
+import copy
 
 def read_ini(inifilepath):
     """Read a given inifile 
@@ -28,13 +35,15 @@ def read_ini(inifilepath):
     otherwise trying to be clever.  *Maybe* have /etc/ location for
     the default file but in general I find that just complicates what
     should be simple, and gets very complicated when dealing with
-    diffenrnt envs 
-
+    diffenrnt envs
+ 
+    >>> testd = {'hello':'sailor', 'section1': {'foo':'bar'}, 'bill':'ben'}
     >>> import config
-    >>> c = config.read_ini('/home/pbrian/.devstation/config.ini')
-    >>> c
-    {'default': {'foo': 'bar', 'balh': '2'}}
-
+    >>> loc = '/tmp/foo.ini' 
+    >>> config.write_ini(testd, loc)
+    >>> confd = config.read_ini(loc)
+    >>> confd == testd # Test dict equalty and ensure round trip
+    True
 
     """
     conf = configparser.ConfigParser()
@@ -42,11 +51,34 @@ def read_ini(inifilepath):
     return convert_conf_to_dict(conf)
 
 def write_ini(confd, inifilepath):
-    """Write a given inifile 
+    """Write a given dict `confd` to inifile format at `inifilepath`.
+
+    confd must be a dict. Any dict items in confd will be treated as
+    *sections* in the ini file, and their key/items will be written
+    under the section name.  However anything else in confd not a dict
+    will be assumed to be in 'default' section.
+
+    default section is stored in ini file but reading will remove
+    default and promote those keys to top level.
+
+    We perfomr deepcopy, because else we will pop / transform the 
+    dict passed in (ie changing something at outer level). THis is 
+    unexpected so we avoid.
 
     """
+    incomingd = copy.deepcopy(confd)
     conf = configparser.ConfigParser()
-    conf['default'] = confd
+    removekeys = []
+    for k,i in incomingd.items():
+        if isinstance(i, dict):
+            conf[k] = i
+            removekeys.append(k)
+    #remove all dict items from incoming confd. (canot change size of
+    #dict whilst iterating over it)
+    for k in removekeys:
+        incomingd.pop(k)
+    #all remaining items into default
+    conf['default'] = incomingd
     with open(inifilepath, 'w') as fo:
         conf.write(fo)
     
@@ -56,8 +88,11 @@ def convert_conf_to_dict(confobj):
     """Convert configparser object to just plain dict """
     d = {}
     for section in confobj._sections:
-        d[section] = {};
-        d[section].update(confobj[section])
+        if section == 'default':
+            d.update(confobj[section])
+        else:
+            d[section] = {};
+            d[section].update(confobj[section])
     return d
 
 if __name__ == '__main__':
